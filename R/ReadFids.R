8ac303bd0d7608494304c25799d0e8f55715587c
Manon Martin <manmartin@oasis.uclouvain.be>
2016-02-09 14:39:52 +0100

    modified the rd file

--- R/ReadFids.R
+++ R/ReadFids.R
@@ -1,8 +1,11 @@
 #' @export ReadFids
-ReadFids <- function(path, l=1) {
+ReadFids <- function(path, l=1, subdirs = FALSE) {
   begin_info <- beginTreatment("ReadFids")
   checkArg(path, c("str"))
   checkArg(l, c("pos"))
+  
+  if (subdirs==FALSE) {
+  
   fidDirs <- getDirsContainingFid(path)
   n <- length(fidDirs)
   if (n == 0L) {
@@ -21,5 +24,36 @@ ReadFids <- function(path, l=1) {
     Fid_data[i,] = fid
     Fid_info[i,] = unlist(info)
   }
+  
+  }else{
+    maindirs = dir(path, full.names=TRUE)
+    Fid_data = numeric()
+    Fid_info = numeric()
+    
+    for (j in maindirs) {
+      fidDirs <- getDirsContainingFid(j)
+      n <- length(fidDirs)
+      if (n == 0L) {
+        stop(paste("No valid fid in", path))
+      }
+      fidNames <- sapply(X = fidDirs, FUN = getTitle, l = l, USE.NAMES=F)
+      for (i in 1:n) {
+        fidList <- ReadFid(fidDirs[i])
+        fid <- fidList[["fid"]]
+        info <- fidList[["params"]]
+        m <- length(fid)
+        if (i == 1) {
+          Fid_DATA <- matrix(nrow=n, ncol=m, dimnames=list(fidNames, info[["DT"]] * (0:(m-1))))
+          Fid_INFO <- matrix(nrow=n, ncol=length(info), dimnames=list(fidNames,names(info)))
+        }
+        Fid_DATA[i,] = fid
+        Fid_INFO[i,] = unlist(info)
+      }
+      Fid_data = rbind(Fid_data, Fid_DATA)
+      Fid_info = rbind(Fid_info, Fid_INFO)
+    }
+    
+  }
+  
   return(list(Fid_data=endTreatment("ReadFids", begin_info, Fid_data), Fid_info=Fid_info))
 }
