b389da7242d53972a83fbfa4aaa6814c3323640a
Manon Martin <manmartin@oasis.uclouvain.be>
2016-04-05 10:50:53 +0200

    Added scaling function

---
+++ R/Scaling.R
@@ -0,0 +1,28 @@
+#' @export Scaling
+Scaling <- function(Spectrum_data, type.scaling=c("pareto", "uv")) {
+  
+  begin_info <- beginTreatment("Scaling", Spectrum_data, force.real=T)
+  Spectrum_data <- begin_info[["Signal_data"]]
+  type.scaling <- match.arg(type.scaling)
+  
+  stdev<-apply(Spectrum_data,2,sd, na.rm = TRUE)
+  
+  invalid <- (stdev == 0)
+  if (TRUE %in% invalid) {
+    invalid_factor <- stdev[invalid]
+    warning("The ", type.scaling, "s are 0 for the spectra ", paste(substr(names(invalid_factor),1,4), collapse=", "), " which is null, the scaling will not happen for them.")
+    stdev[invalid] <- 1
+  }
+  
+  switch(type.scaling,
+         "uv" = { 
+           Scaled.spectrum <- sweep(Spectrum_data,2,stdev,"/")
+         },
+         "pareto" = { 
+           Scaled.spectrum <- sweep(Spectrum_data,2,sqrt(stdev),"/")
+         }
+         
+  )
+return(endTreatment("Scaling", begin_info, Scaled.spectrum))
+       
+}
