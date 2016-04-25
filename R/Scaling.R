ba9d6029d5abfe43e76c0690c6a828c833764907
Manon Martin <manmartin@oasis.uclouvain.be>
2016-04-25 16:01:46 +0200

    added centering of variables

--- R/Scaling.R
+++ R/Scaling.R
@@ -1,5 +1,5 @@
 #' @export Scaling
-Scaling <- function(Spectrum_data, type.scaling=c("pareto", "uv")) {
+Scaling <- function(Spectrum_data, type.scaling=c("center", "pareto", "uv")) {
   
   begin_info <- beginTreatment("Scaling", Spectrum_data, force.real=T)
   Spectrum_data <- begin_info[["Signal_data"]]
@@ -8,20 +8,24 @@ Scaling <- function(Spectrum_data, type.scaling=c("pareto", "uv")) {
   stdev<-apply(Spectrum_data,2,sd, na.rm = TRUE)
   
   invalid <- (stdev == 0)
-  if (TRUE %in% invalid) {
+  if (TRUE %in% invalid & type.scaling != "center") {
     invalid_factor <- stdev[invalid]
     warning("The ", type.scaling, "s are 0 for the spectra ", paste(substr(names(invalid_factor),1,4), collapse=", "), " which is null, the scaling will not happen for them.")
     stdev[invalid] <- 1
   }
   
   switch(type.scaling,
-         "uv" = { 
-           Scaled.spectrum <- sweep(Spectrum_data,2,stdev,"/")
+         "center" = { 
+           Scaled.spectrum <- apply(Spectrum_data,2, (x - mean(x)))
          },
-         "pareto" = { 
-           Scaled.spectrum <- sweep(Spectrum_data,2,sqrt(stdev),"/")
-         }
          
+         "uv" = { 
+           Scaled.spectrum <- apply(Spectrum_data,2, function(x) (x - mean(x)) / sd(x))
+         },
+         
+         "pareto" = { 
+           Scaled.spectrum <- apply(Spectrum_data,2, function(x) (x - mean(x)) / sqrt(sd(x)))
+         }
   )
 return(endTreatment("Scaling", begin_info, Scaled.spectrum))
        
