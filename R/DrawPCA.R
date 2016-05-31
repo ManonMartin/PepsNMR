1f2f10f80e222c39888f610ff5b07c0692c31fe8
Manon Martin <manmartin@oasis.uclouvain.be>
2016-05-31 17:22:09 +0200

    Improved PCA plot

--- R/DrawPCA.R
+++ R/DrawPCA.R
@@ -1,14 +1,49 @@
 #' @export DrawPCA
-DrawPCA <- function (Signal_data, drawNames=TRUE, createWindow=F) {
+DrawPCA <- function (Signal_data, drawNames=TRUE, createWindow=F, class = NULL, axes =c(1,2)) {
+  checkArg(group_delay, c("num", "pos0"), can.be.null=TRUE)
+  
+  if(0 %in%class){
+    class = class+1
+  }
+  
+  Xax = axes[1]
+  Yax = axes[2]
+  
   pca <- prcomp(Re(Signal_data))
+  
+  # Eigenvalues
+  eig <- (pca$sdev)^2
+  
+  # Variances in percentage
+  variance <- eig*100/sum(eig)
+  
   if (nrow(Signal_data) < 2) {
     stop("At least 2 spectra are needed for PCA.")
   }
   if (createWindow) {
     dev.new(noRStudioGD = TRUE)
   }
-  plot(pca$x[,1], pca$x[,2], xlab="PC1", ylab="PC2")
-  if (drawNames) {
-    text(pca$x[,1], pca$x[,2], rownames(Signal_data), pos=3)
+  Xlim=c(min(pca$x[,Xax])*1.4, max(pca$x[,Xax])*1.4)
+  Ylim=c(min(pca$x[,Yax])*1.4, max(pca$x[,Yax])*1.4)
+  
+  
+  if(is.null(class)) {
+    plot(pca$x[,Xax], pca$x[,Yax], xlab="PC1", ylab="PC2")
+    if (drawNames) {
+      text(pca$x[,Xax], pca$x[,Yax], rownames(Signal_data), pos=c(2,3))
+    }
+  }else{
+    
+    
+    plot(pca$x[,Xax], pca$x[,Yax], col=class,
+         xlab=paste0("PC",Xax," (", round(variance[Xax],2) ,"%)"), xlim=Xlim,
+         ylab=paste0("PC",Yax," (", round(variance[Yax],2) ,"%)"), ylim=Ylim,
+         main=paste0("PCA score plot"))
+    
+    if (drawNames) {
+      text(pca$x[,Xax],pca$x[,Yax],labels=rownames(Signal_data), pos=c(2,3), col=class)
+    }
   }
+  
+  
 }
