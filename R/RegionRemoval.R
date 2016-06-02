5621bbba0c4c681115093cf0a33fd40ecf82215a
Manon Martin <manmartin@oasis.uclouvain.be>
2016-06-02 15:58:19 +0200

    fixed function

--- R/RegionRemoval.R
+++ R/RegionRemoval.R
@@ -9,22 +9,24 @@ RegionRemoval <- function(Spectrum_data, typeofspectra = c(NULL, "Serum", "Urine
   typeofspectra = match.arg(typeofspectra)
   ppm <- as.numeric(colnames(Spectrum_data))
   
-  if (is.null(typeofspectra)) {
-    if (is.null(fromto.rr)) {
-      stop(paste("fromto.rr argument is null with no default values"))
-    } 
-    diff = diff(unlist(fromto.rr))[1:length(diff(unlist(fromto.rr))) %% 2 != 0]
-    for (i in 1:length(diff)) {
-      if (diff[i]<=0) {
-        stop(paste("Invalid region removal because from > to"))
-        } 
+  if (typeofspectra == "Urine")  {
+    fromto.rr=list(Water =c(4.8, 5.2), Uree=c(5.5, 6))
+    } else if (typeofspectra == "Serum") {
+      fromto.rr=list(Water =c(4.8, 5.2))
+    } else {
+     if (is.null(fromto.rr)) {
+       stop(paste("fromto.rr argument is null with no default values"))
+     } 
+     diff = diff(unlist(fromto.rr))[1:length(diff(unlist(fromto.rr))) %% 2 != 0]
+     for (i in 1:length(diff)) {
+       if (diff[i]<=0) {
+         stop(paste("Invalid region removal because from > to"))
+       } 
+     }
     }
 
-  } else if (typeofspectra == "Serum") {
-      fromto.rr=list(Water =c(4.8, 5.2))
-      
-  } else {fromto.rr=list(Water =c(4.8, 5.2), Uree=c(5.5, 6))}
-
+  
+  
   
 interval = vector("list", length(fromto.rr))
   for (i in 1:length(fromto.rr)) {
